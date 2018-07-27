effaea081e426f13e370dd51ee4567572136ea06
ridickle7 <ridickle7@gmail.com>
2018-07-27 12:22:29 +0900

    Initial commit

---
+++ app/src/main/java/filaroid/co/kr/camera_exercise/MainActivity.kt
@@ -0,0 +1,12 @@
+package filaroid.co.kr.camera_exercise
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
